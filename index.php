23bf17b21ab33755488c61277067c510fbaa84c9
MiraD1n <mupolischuk@gmail.com>
2019-09-16 17:39:15 +0300

    work circle bage

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
-$link = mysqli_connect("127.0.0.1", "mx123", "Qq123456", "mirad1n");
 
-if (!$link) {
-    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+$mysqli = new mysqli("127.0.0.1", "mx123", "Qq123456", "mirad1n");
+
+/* проверка соединения */
+if ($mysqli->connect_errno) {
+    printf("Не удалось подключиться: %s\n", $mysqli->connect_error);
+    exit();
+
 }
+else echo ("Connection DONE!");
 
-echo "Соединение с MySQL установлено!" . PHP_EOL;
-echo "Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
+$result = $mysqli->query("SELECT * FROM bages WHERE id='1'");
+$row = $result->fetch_assoc();
+$t = $row["img_lk"];
+echo $t;
+
 ?>
-
+<p><img src="1.png" alt="Письма мастера дзен"></p>
 <h1>TEST</h1>
 
 <?php
-echo "commit1";
-mysql_free_result($result);
-mysqli_close($link);
+
+$mysqli->close();
 ?>
---
+++ test.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+  <style type="text/css">
+  .block {
+    width: 300px;
+    height: 252px;
+    margin: 30px auto;
+  }
+
+  .block svg {
+    width: 100%;
+    height: 100%;
+    stroke-width: 1px;
+    stroke: #000;
+  }
+  </style>
+</head>
+<div class="block">
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 252">
+  <defs>
+    <pattern id="img" patternUnits="userSpaceOnUse" width="100%" height="100%">
+      <image xlink:href="http://backgroundlabs.com/wp-content/uploads/2013/03/dandelion-seeds-pattern.png" />
+    </pattern>
+  </defs>
+    <path fill="url(#img)" d="M75.173,251.959L0,126.152l74.827-126l150-0.193L300,125.766l-74.827,126L75.173,251.959z"/>
+</svg>
+</div>
+</html>
